016e9b4a0973fa5320350c0f1ccb342ab78a1d9a
Unknown <danielfernandez31295@hotmail.es>
2017-04-24 16:49:52 +0200

    Fitxers bbdd + classes Dani

---
+++ Project/model/business/class_Missatge.php
@@ -0,0 +1,75 @@
+<?php
+
+
+class Missatge{
+    private $id_missatge;
+    private $id_usuari;
+    private $llegit;
+    private $titol;
+    private $data;
+    private $missatge;
+
+    function __construct($id_usuari, $llegit, $titol, $data, $missatge) {
+        $this->id_missatge = null;
+        $this->id_usuari = $this->setId_usuari($id_usuari);
+        $this->llegit = $this->setLlegit($llegit);
+        $this->titol = $this->setTitol($titol);
+        $this->data = $this->setData($data);
+        $this->missatge = $this->setMissatge($missatge);
+    }
+    
+    
+    function getId_missatge() {
+        return $this->id_missatge;
+    }
+
+    function getId_usuari() {
+        return $this->id_usuari;
+    }
+
+    function getLlegit() {
+        return $this->llegit;
+    }
+
+    function getTitol() {
+        return $this->titol;
+    }
+
+    function getData() {
+        return $this->data;
+    }
+
+    function getMissatge() {
+        return $this->missatge;
+    }
+
+    function setId_missatge($id_missatge) {
+        $this->id_missatge = $id_missatge;
+    }
+
+    function setId_usuari($id_usuari) {
+        $this->id_usuari = $id_usuari;
+    }
+
+    function setLlegit($llegit) {
+        $this->llegit = $llegit;
+    }
+
+    function setTitol($titol) {
+        $this->titol = $titol;
+    }
+
+    function setData($data) {
+        $this->data = $data;
+    }
+
+    function setMissatge($missatge) {
+        $this->missatge = $missatge;
+    }
+
+
+    
+
+
+}
+
