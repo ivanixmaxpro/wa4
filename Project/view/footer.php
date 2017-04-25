50f0f4753c5fad8c570aaf0700a67df61a6d4671
Unknown <ivan.mir.014@gmail.com>
2017-04-25 17:38:35 +0200

    Views Empleats

--- Project/view/footer.php
+++ Project/view/footer.php
@@ -1,3 +1,21 @@
+
+<script type="text/javascript">
+    $(document).ready(function(){
+
+        demo.initChartist();
+
+        $.notify({
+            icon: 'pe-7s-gift',
+            message: "Lanzar esta notificación solo si el usuario tiene mensajes pendientes. (footer)"
+
+        },{
+            type: 'info',
+            timer: 4000
+        });
+
+    });
+</script>
+</body>
     </div>
 </body>
 </html>
