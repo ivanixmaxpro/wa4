6beadcc06794dd172300f75bdfe258ebc77f6d6f
Unknown <ernest.riccetto@gmail.com>
2017-04-27 15:18:01 +0200

    usuari usuaridao, login_ctl modificat

--- Project/model/persistence/class_UsuariDAO.php
+++ Project/model/persistence/class_UsuariDAO.php
@@ -10,6 +10,24 @@ class UsuariDAO {
 		$con = new db();
 		$con->consulta($query);
 		$con->close();
-	} 
+	}
+    public function populateUsuariDAO (){
+        $usuaris = array();
+        $con = new db();
+        $query = $con->prepare("SELECT * FROM usuari;");
+        $result = $con->consultar($query);
+
+        foreach ($result as $row) {
+            $id_usuari = $row["id_usuari"];
+            $id_empleat = $row["id_empleat"];
+            $usuari = $row["usuari"];
+            $contrasenya = $row["contrasenya"];
+            $usuari = new Usuari($id_usuari, $id_empleat, $usuari, $contrasenya);
+            array_push($usuaris, $usuari);
+        }
+
+        $con = null;
+        return $usuaris;
+    }
 }
 ?>
