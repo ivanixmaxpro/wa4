14ac4ac5a8b98a81a147d0259f8314bac5c468a3
Unknown <ernest.riccetto@gmail.com>
2017-05-02 15:28:44 +0200

    productes wip

--- Project/view/llistaProductes.php
+++ Project/view/llistaProductes.php
@@ -4,38 +4,46 @@
         <h4 class="title">Llista productes</h4>
         <p class="category">A continuació un llistat:</p>
     </div>
-    <div class="content table-responsive table-full-width">
-        <table class="table table-hover table-striped">
-            <thead>
-            <tr><th>ID</th>
-                <th>Nom</th>
-                <th>marca</th>
-                <th>model</th>
-                <th>preuBase</th>
-                <th></th>
-            </tr></thead>
-            <tbody>
+    <form action="?ctl=actors" method="post">
+        <div class="content table-responsive table-full-width">
+            <table class="table table-hover table-striped">
+                <thead>
+                    <tr><th>ID</th>
+                        <th>Nom</th>
+                        <th>marca</th>
+                        <th>preuBase</th>
+                        th>referència</th>
+                        <th>model</th>
+                        <th>conservar en fred</th>
+                        <th>detall</th>
+                        <th>modificar</th>
+                        <th>eliminar</th>
+                    </tr></thead>
+                <tbody>
 
-            <?php
-            
-            foreach ($productes as $row) {
-               
-                echo '<tr>';
-                echo "<td>" .$row->getId_producte()  . "</td>";
-                echo "<td>" .$row->getNom()  . "</td>";
-                echo "<td>" .$row->getMarca()  . "</td>";
-                echo "<td>" .$row->getModel()  . "</td>";
-                echo "<td>" .$row->getpreuBase()  . "</td>";
-                echo '<td>'.'<a href="?ctl=producte&act=detall&id='.$row->getId_producte().'">'.'Veure'.'</a>'.'</td>';
-                echo "</tr>";
-                
-                
-            }
-            ?>
+                    <?php
+                    foreach ($productes as $row) {
 
-            </tbody>
-        </table>
+                        echo '<tr>';
+                        echo "<td>" . $row->getId_producte() . "</td>";
+                        echo "<td>" . $row->getNom() . "</td>";
+                        echo "<td>" . $row->getMarca() . "</td>";
+                        echo "<td>" . $row->getpreuBase() . "</td>";
+                        echo "<td>" . $row->getReferencia() . "</td>";
+                        echo "<td>" . $row->getModel() . "</td>";
+                        echo "<td>" . $row->getConservarEnFred() . "</td>";
+                        echo '<td>' . '<a href="?ctl=producte&act=detall&id=' . $row->getId_producte() . '">' . 'Veure' . '</a>' . '</td>';
+                        ?>   <a href="?ctl=producte&act=modificar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Modificar producte</a>
+                        <?php echo "</tr>";
+                        ?>   <a href="?ctl=producte&act=eliminar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Eliminar producte</a>
+                    <?php
+                    echo "</tr>";
+                }
+                ?>
 
-    </div>
+                </tbody>
+            </table>
+        </div>
+    </form>  
 </div>
 </div>
