bf3fa0b45ae4d63d15a27c00e4c1ca51347fe6da
Unknown <ernest.riccetto@gmail.com>
2017-05-02 19:54:58 +0200

    vista i controller llista productes

--- Project/controller/llistaProductes_ctl.php
+++ Project/controller/llistaProductes_ctl.php
@@ -1,32 +1,58 @@
 <?php
-//Empresa	
-if(isset($_SESSION['empresa'])){
-	$empresa = unserialize($_SESSION['empresa']);
+require_once 'view/tablaProducte.php';
+//Empresa
+$tipus = "tots";
+if (isset($_SESSION['empresa'])) {
+    $empresa = unserialize($_SESSION['empresa']);
 } else {
-	$empresa = New Empresa();
-	$empresa->recuperarEmpresa(); 
+    $empresa = New Empresa();
+    $empresa->recuperarEmpresa();
 
-	$_SESSION['empresa'] = serialize($empresa);
+    $_SESSION['empresa'] = serialize($empresa);
 }
 
 if (isset($_REQUEST["Submit"])) {
-    if (isset($_REQUEST['marca'])) {
-        $marca = $_REQUEST['marca'];
-    }
+
     if (isset($_REQUEST['conservarFred'])) {
         $conservarFred = $_REQUEST['conservarFred'];
     }
     if (isset($_REQUEST['quantitat'])) {
-        $quantitat = $_REQUEST['quantitat'];      
+        $quantitat = $_REQUEST['quantitat'];
     }
-    $productes = $empresa->cercar($marca, $conservarFred,$quantitat);
-}else{
-   $productes = $empresa->populateProductes();
+    if (isset($_REQUEST['tipus'])) {
+        $tipus = $_REQUEST['tipus'];
+        switch ($tipus) {
+            case "altres":
+                $productesAltres = $empresa->populateAltres();
+                $productes = $empresa->cercarProducte($conservarFred, $quantitat,$productesAltres);
+                break;
+            case "solid":
+                $productesSolid = $empresa->populateSolid();
+                $productes = $empresa->cercarProducte($conservarFred, $quantitat,$productesSolid);
+                break;
+            case "semisolid":
+                $productesSemiSolid = $empresa->populateSemiSolid();
+                $productes = $empresa->cercarProducte($conservarFred, $quantitat,$productesSemiSolid);
+                break;
+            case "liquid":
+                $productesLiquid = $empresa->populateLiquid();
+                $productes = $empresa->cercarProducte($conservarFred, $quantitat,$productesLiquid);
+                break;
+            case "gas":
+                $productesGas = $empresa->populateGas();
+                $productes = $empresa->cercarProducte($conservarFred, $quantitat,$productesGas);
+                break;
+        }
+    }
+    $productestots =$empresa->populateProductes();
+    $productes = $empresa->cercarProducte($conservarFred, $quantitat, $productestots);
+} else {
+    $productes = $empresa->populateProductes();
 }
 require_once 'view/header.php';
 require_once 'view/sidebar.php';
 require_once 'view/mainNav.php';
 require_once 'view/llistaProductes.php';
-require_once 'view/footer.php';
 
+require_once 'view/footer.php';
 ?>
