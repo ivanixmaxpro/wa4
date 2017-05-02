51e153d4989a69edf511749583414c1a61a00032
Unknown <psppau@hotmail.com>
2017-05-02 20:05:32 +0200

    TablaProducte

--- Project/view/tablaProducte.php
+++ Project/view/tablaProducte.php
@@ -1,28 +1,28 @@
 <?php
 
 /**
- * tabla productes solid
- * @param type array productes
+ * tabla productes solid 
+ * @param type array productes 
  */
 function tablaSemiSolid($productes) {
-    ?>
-    <div class="content table-responsive table-full-width">
-        <table class="table table-hover table-striped">
-            <thead>
-                <tr><th>ID</th>
-                    <th>referència</th>
-                    <th>Nom</th>
-                    <th>marca</th>
-                    <th>model</th>
-                    <th>preuBase</th>
-                    <th>conservar en fred</th>
-                    <th>capacitat mg</th>
-                    
-                    <th>detall</th>
-                    <th>modificar</th>
-                    <th>eliminar</th>
-                </tr></thead>
-            <tbody>
+    ?> 
+    <div class="content table-responsive table-full-width"> 
+        <table class="table table-hover table-striped"> 
+            <thead> 
+                <tr><th>ID</th> 
+                    <th>referència</th> 
+                    <th>Nom</th> 
+                    <th>marca</th> 
+                    <th>model</th> 
+                    <th>preuBase</th> 
+                    <th>conservar en fred</th> 
+                    <th>capacitat mg</th> 
+
+                    <th>detall</th> 
+                    <th>modificar</th> 
+                    <th>eliminar</th> 
+                </tr></thead> 
+            <tbody> 
 
                 <?php
                 foreach ($productes as $row) {
@@ -34,50 +34,50 @@ function tablaSemiSolid($productes) {
                     echo "<td>" . $row->getpreuBase() . "</td>";
                     echo "<td>" . $row->getReferencia() . "</td>";
                     echo "<td>" . $row->getModel() . "</td>";
-                    echo "<td>" . $row->getConservarEnFred() . "</td>";
+                    echo "<td>" . $row->getConservarFred() . "</td>";
                     echo "<td>" . $row->getCapacitatMg() . "</td>";
-                    
+
                     echo '<td>' . '<a href="?ctl=producte&act=detall&id=' . $row->getId_producte() . '">' . 'Veure' . '</a>' . '</td>';
-                    ?>   <a href="?ctl=producte&act=modificar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Modificar producte</a>
-                <?php echo "</tr>";
-                ?>   <a href="?ctl=producte&act=eliminar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Eliminar producte</a>
+                    ?>   <a href="?ctl=producte&act=modificar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Modificar producte</a> 
+                    <?php echo "</tr>";
+                    ?>   <a href="?ctl=producte&act=eliminar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Eliminar producte</a> 
                 <?php
                 echo "</tr>";
             }
-            ?>
+            ?> 
 
-            </tbody>
-        </table>
-    </div>
+            </tbody> 
+        </table> 
+    </div> 
     <?php
 }
-?>
+?> 
 
 <?php
 
 /**
- * tabla productes solid
- * @param type array productes
+ * tabla productes solid 
+ * @param type array productes 
  */
 function tablaSolid($productes) {
-    ?>
-    <div class="content table-responsive table-full-width">
-        <table class="table table-hover table-striped">
-            <thead>
-                <tr><th>ID</th>
-                    <th>referència</th>
-                    <th>Nom</th>
-                    <th>marca</th>
-                    <th>model</th>
-                    <th>preuBase</th>
-                    <th>conservar en fred</th>
-                    <th>capacitat mg</th>
-                    <th>unitats</th>
-                    <th>detall</th>
-                    <th>modificar</th>
-                    <th>eliminar</th>
-                </tr></thead>
-            <tbody>
+    ?> 
+    <div class="content table-responsive table-full-width"> 
+        <table class="table table-hover table-striped"> 
+            <thead> 
+                <tr><th>ID</th> 
+                    <th>referència</th> 
+                    <th>Nom</th> 
+                    <th>marca</th> 
+                    <th>model</th> 
+                    <th>preuBase</th> 
+                    <th>conservar en fred</th> 
+                    <th>capacitat mg</th> 
+                    <th>unitats</th> 
+                    <th>detall</th> 
+                    <th>modificar</th> 
+                    <th>eliminar</th> 
+                </tr></thead> 
+            <tbody> 
 
                 <?php
                 foreach ($productes as $row) {
@@ -89,48 +89,48 @@ function tablaSolid($productes) {
                     echo "<td>" . $row->getpreuBase() . "</td>";
                     echo "<td>" . $row->getReferencia() . "</td>";
                     echo "<td>" . $row->getModel() . "</td>";
-                    echo "<td>" . $row->getConservarEnFred() . "</td>";
+                    echo "<td>" . $row->getConservarFred() . "</td>";
                     echo "<td>" . $row->getCapacitatMg() . "</td>";
                     echo "<td>" . $row->getUnitats() . "</td>";
                     echo '<td>' . '<a href="?ctl=producte&act=detall&id=' . $row->getId_producte() . '">' . 'Veure' . '</a>' . '</td>';
-                    ?>   <a href="?ctl=producte&act=modificar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Modificar producte</a>
-                <?php echo "</tr>";
-                ?>   <a href="?ctl=producte&act=eliminar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Eliminar producte</a>
+                    ?>   <a href="?ctl=producte&act=modificar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Modificar producte</a> 
+                    <?php echo "</tr>";
+                    ?>   <a href="?ctl=producte&act=eliminar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Eliminar producte</a> 
                 <?php
                 echo "</tr>";
             }
-            ?>
+            ?> 
 
-            </tbody>
-        </table>
-    </div>
+            </tbody> 
+        </table> 
+    </div> 
     <?php
 }
-?>
+?> 
 
 <?php
 
 /**
- * tabla general de productes
- * @param type  array de productes
+ * tabla general de productes 
+ * @param type  array de productes 
  */
 function tablaTot($productes) {
-    ?>
-    <div class="content table-responsive table-full-width">
-        <table class="table table-hover table-striped">
-            <thead>
-                <tr><th>ID</th>
-                    <th>referència</th>
-                    <th>Nom</th>
-                    <th>marca</th>
-                    <th>model</th>
-                    <th>preuBase</th>
-                    <th>conservar en fred</th>
-                    <th>detall</th>
-                    <th>modificar</th>
-                    <th>eliminar</th>
-                </tr></thead>
-            <tbody>
+    ?> 
+    <div class="content table-responsive table-full-width"> 
+        <table class="table table-hover table-striped"> 
+            <thead> 
+                <tr><th>ID</th> 
+                    <th>referència</th> 
+                    <th>Nom</th> 
+                    <th>marca</th> 
+                    <th>model</th> 
+                    <th>preuBase</th> 
+                    <th>conservar en fred</th> 
+                    <th>detall</th> 
+                    <th>modificar</th> 
+                    <th>eliminar</th> 
+                </tr></thead> 
+            <tbody> 
 
                 <?php
                 foreach ($productes as $row) {
@@ -142,47 +142,47 @@ function tablaTot($productes) {
                     echo "<td>" . $row->getpreuBase() . "</td>";
                     echo "<td>" . $row->getReferencia() . "</td>";
                     echo "<td>" . $row->getModel() . "</td>";
-                    echo "<td>" . $row->getConservarEnFred() . "</td>";
+                    echo "<td>" . $row->getConservarFred() . "</td>";
                     echo '<td>' . '<a href="?ctl=producte&act=detall&id=' . $row->getId_producte() . '">' . 'Veure' . '</a>' . '</td>';
-                    ?>   <a href="?ctl=producte&act=modificar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Modificar producte</a>
-                <?php echo "</tr>";
-                ?>   <a href="?ctl=producte&act=eliminar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Eliminar producte</a>
+                    ?>   <a href="?ctl=producte&act=modificar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Modificar producte</a> 
+                    <?php echo "</tr>";
+                    ?>   <a href="?ctl=producte&act=eliminar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Eliminar producte</a> 
                 <?php
                 echo "</tr>";
             }
-            ?>
+            ?> 
 
-            </tbody>
-        </table>
-    </div>
+            </tbody> 
+        </table> 
+    </div> 
     <?php
 }
-?>
+?> 
 
 <?php
 
 /**
- * tabla  productes liquids
- * @param type  array de productes
+ * tabla  productes liquids 
+ * @param type  array de productes 
  */
 function tablaLiquid($productes) {
-    ?>
-    <div class="content table-responsive table-full-width">
-        <table class="table table-hover table-striped">
-            <thead>
-                <tr><th>ID</th>
-                    <th>referència</th>
-                    <th>Nom</th>
-                    <th>marca</th>
-                    <th>model</th>
-                    <th>preuBase</th>
-                    <th>conservar en fred</th>
-                    <th>capacitat ml</th>
-                    <th>detall</th>
-                    <th>modificar</th>
-                    <th>eliminar</th>
-                </tr></thead>
-            <tbody>
+    ?> 
+    <div class="content table-responsive table-full-width"> 
+        <table class="table table-hover table-striped"> 
+            <thead> 
+                <tr><th>ID</th> 
+                    <th>referència</th> 
+                    <th>Nom</th> 
+                    <th>marca</th> 
+                    <th>model</th> 
+                    <th>preuBase</th> 
+                    <th>conservar en fred</th> 
+                    <th>capacitat ml</th> 
+                    <th>detall</th> 
+                    <th>modificar</th> 
+                    <th>eliminar</th> 
+                </tr></thead> 
+            <tbody> 
 
                 <?php
                 foreach ($productes as $row) {
@@ -194,48 +194,48 @@ function tablaLiquid($productes) {
                     echo "<td>" . $row->getpreuBase() . "</td>";
                     echo "<td>" . $row->getReferencia() . "</td>";
                     echo "<td>" . $row->getModel() . "</td>";
-                    echo "<td>" . $row->getConservarEnFred() . "</td>";
+                    echo "<td>" . $row->getConservarFred() . "</td>";
                     echo "<td>" . $row->getCapacitatMl() . "</td>";
                     echo '<td>' . '<a href="?ctl=producte&act=detall&id=' . $row->getId_producte() . '">' . 'Veure' . '</a>' . '</td>';
-                    ?>   <a href="?ctl=producte&act=modificar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Modificar producte</a>
-                <?php echo "</tr>";
-                ?>   <a href="?ctl=producte&act=eliminar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Eliminar producte</a>
+                    ?>   <a href="?ctl=producte&act=modificar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Modificar producte</a> 
+                    <?php echo "</tr>";
+                    ?>   <a href="?ctl=producte&act=eliminar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Eliminar producte</a> 
                 <?php
                 echo "</tr>";
             }
-            ?>
+            ?> 
 
-            </tbody>
-        </table>
-    </div>
+            </tbody> 
+        </table> 
+    </div> 
     <?php
 }
-?>
+?> 
 
 <?php
 
 /**
- * tabla  productes liquids
- * @param type  array de productes
+ * tabla  productes liquids 
+ * @param type  array de productes 
  */
 function tablaGas($productes) {
-    ?>
-    <div class="content table-responsive table-full-width">
-        <table class="table table-hover table-striped">
-            <thead>
-                <tr><th>ID</th>
-                    <th>referència</th>
-                    <th>Nom</th>
-                    <th>marca</th>
-                    <th>model</th>
-                    <th>preuBase</th>
-                    <th>conservar en fred</th>
-                    <th>capacitat ml</th>
-                    <th>detall</th>
-                    <th>modificar</th>
-                    <th>eliminar</th>
-                </tr></thead>
-            <tbody>
+    ?> 
+    <div class="content table-responsive table-full-width"> 
+        <table class="table table-hover table-striped"> 
+            <thead> 
+                <tr><th>ID</th> 
+                    <th>referència</th> 
+                    <th>Nom</th> 
+                    <th>marca</th> 
+                    <th>model</th> 
+                    <th>preuBase</th> 
+                    <th>conservar en fred</th> 
+                    <th>capacitat ml</th> 
+                    <th>detall</th> 
+                    <th>modificar</th> 
+                    <th>eliminar</th> 
+                </tr></thead> 
+            <tbody> 
 
                 <?php
                 foreach ($productes as $row) {
@@ -247,20 +247,19 @@ function tablaGas($productes) {
                     echo "<td>" . $row->getpreuBase() . "</td>";
                     echo "<td>" . $row->getReferencia() . "</td>";
                     echo "<td>" . $row->getModel() . "</td>";
-                    echo "<td>" . $row->getConservarEnFred() . "</td>";
+                    echo "<td>" . $row->getConservarFred() . "</td>";
                     echo "<td>" . $row->getCapacitatMl() . "</td>";
                     echo '<td>' . '<a href="?ctl=producte&act=detall&id=' . $row->getId_producte() . '">' . 'Veure' . '</a>' . '</td>';
-                    ?>   <a href="?ctl=producte&act=modificar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Modificar producte</a>
-                <?php echo "</tr>";
-                ?>   <a href="?ctl=producte&act=eliminar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Eliminar producte</a>
+                    ?>   <a href="?ctl=producte&act=modificar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Modificar producte</a> 
+                    <?php echo "</tr>";
+                    ?>   <a href="?ctl=producte&act=eliminar&id=<?php echo $row->getId_producte(); ?>" class="btn btn-danger btn-sm"></span> Eliminar producte</a> 
                 <?php
                 echo "</tr>";
             }
-            ?>
+            ?> 
 
-            </tbody>
-        </table>
-    </div>
+            </tbody> 
+        </table> 
+    </div> 
     <?php
 }
-?>
