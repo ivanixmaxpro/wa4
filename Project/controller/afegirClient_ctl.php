00af51e15328c5503a27c6c4f21ea5657bc178c0
Unknown <ernest.riccetto@gmail.com>
2017-05-12 15:59:42 +0200

    clients arreglat amb el nou constructor
    sidebar amb clients i proveidors

--- Project/controller/afegirClient_ctl.php
+++ Project/controller/afegirClient_ctl.php
@@ -28,7 +28,7 @@ if (isset($_REQUEST['Submit'])) {
         $informacio = $_REQUEST['informacio'];
     }
 
-    $client = new Client($nom, $codi,$informacio);
+    $client = new Client(null,$nom, $codi,$informacio);
     $clientDAO->inserir($client);
     $missatge = 'client afegit';
     $redireccio = 'index.php?ctl=client&act=llista';
