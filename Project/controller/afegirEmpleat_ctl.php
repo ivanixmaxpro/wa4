c63d6dc923da95c061d199a318f939b05e3c9d6c
Unknown <ivan.mir.014@gmail.com>
2017-05-16 17:47:01 +0200

    vista afegir empleat

--- Project/controller/afegirEmpleat_ctl.php
+++ Project/controller/afegirEmpleat_ctl.php
@@ -1,6 +1,6 @@
 <?php
 
-$title = "afegir client";
+$title = "afegir empleat";
 require_once 'view/header.php';
 require_once 'view/sidebar.php';
 require_once 'view/mainNav.php';
@@ -16,25 +16,7 @@ if (isset($_SESSION['empresa'])) {
 $clientDAO = new ClientDAO();
 
 
-if (isset($_REQUEST['Submit'])) {
+    require_once 'view/afegirEmpleat.php';
 
-    if (isset($_REQUEST['codi'])) {
-        $codi = $_REQUEST['codi'];
-    }
-    if (isset($_REQUEST['nom'])) {
-        $nom = $_REQUEST['nom'];
-    }
-    if (isset($_REQUEST['informacio'])) {
-        $informacio = $_REQUEST['informacio'];
-    }
-
-    $client = new Client(null,$nom, $codi,$informacio);
-    $clientDAO->inserir($client);
-    $missatge = 'client afegit';
-    $redireccio = 'index.php?ctl=client&act=llista';
-    require_once 'view/confirmacio.php';
-} else {
-    require_once 'view/afegirClient.php';
-}
 require_once 'view/footer.php';
 ?>
